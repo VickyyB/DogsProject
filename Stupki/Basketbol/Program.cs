34b2217d7b0f674f372704e0c5acf9cd851e92f3
VickyyB <viktoria.bozhdarova@gmail.com>
2023-02-06 09:09:57 +0200

    VisualStudio

---
+++ Stupki/Basketbol/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Basketbol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double razhodi = double.Parse(Console.ReadLine());
+            double kecove = razhodi * 0.6;
+            double ekip = kecove * 0.8;
+            double topka = ekip * 0.25;
+            double aksesoari = topka * 0.2;
+            double cena = razhodi + kecove + ekip + topka + aksesoari;
+            Console.WriteLine($"{cena:F2}");
+
+        }
+    }
+}
---
+++ Stupki/Stupki/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Stupki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int obshtoStupki = 0;
+            bool postignaCel = false;
+
+            while (input != "Going home")
+            {
+                int stupki = int.Parse(input);
+                obshtoStupki += stupki;
+
+                if (obshtoStupki >= 10000)
+                {
+                    postignaCel = true;
+                    break;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            if (postignaCel)
+            {
+                int povecheStupki = obshtoStupki - 10000;
+
+                Console.WriteLine("Goal reached! Good job!");
+                Console.WriteLine($"{povecheStupki} steps over the goal!");
+            }
+            else
+            {
+                int additionalStupki = int.Parse(Console.ReadLine());
+                obshtoStupki += additionalStupki;
+
+                if (obshtoStupki >= 10000)
+                {
+                    int povecheStupki = obshtoStupki - 10000;
+
+                    Console.WriteLine("Goal reached! Good job!");
+                    Console.WriteLine($"{povecheStupki} steps over the goal!");
+                }
+                else
+                {
+                    int stepsToGoal = 10000 - obshtoStupki;
+                    Console.WriteLine($"{stepsToGoal} more steps to reach goal.");
+                }
+            }
+        }
+    }
+}
